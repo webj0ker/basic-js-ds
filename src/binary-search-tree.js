6a6717391ccfa6595b010d6bb6f20cc951b1a170
Stas <stanislav.shendryk@gmail.com>
2021-10-19 01:00:01 +0300

    feat: add binary-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -16,16 +16,42 @@ module.exports = class BinarySearchTree {
         return this.binaryRoot;
     }
 
-    add(data) {
+    addNode(node, data) {
 
+        if (!node) {
+            return new Node(data);
+        }
+        if (node.data === data) {
+            return node;
+        }
+
+        node.data > data ? node.left = this.addNode(node.left, data) : node.right = this.addNode(node.right, data);
+        return node;
+    }
+
+
+    add(data) {
+        this.binaryRoot = this.addNode(this.binaryRoot, data);
+    }
+
+
+
+    search(node, data) {
+        if (node === null) {
+            return null;
+        } else if (data < node.data) {
+            return this.search(node.left, data);
+        } else if (data > node.data) {
+            return this.search(node.right, data);
+        } else return node;
     }
 
     has(data) {
-
+        return this.search(this.binaryRoot, data) !== null;
     }
 
     find(data) {
-
+        return this.search(this.binaryRoot, data);
     }
 
     remove(data) {
@@ -34,10 +60,27 @@ module.exports = class BinarySearchTree {
 
     min() {
 
-    }
+        let currentNode = this.binaryRoot;
+        if (currentNode) {
+            while (currentNode.left) {
+                currentNode = currentNode.left;
+            }
+            return currentNode.data;
+        } else {
+            return null;
+        }
 
+    }
     max() {
 
+        let currentNode = this.binaryRoot;
+        if (currentNode) {
+            while (currentNode.right) {
+                currentNode = currentNode.right;
+            }
+            return currentNode.data;
+        } else {
+            return null;
+        }
     }
-
 }
